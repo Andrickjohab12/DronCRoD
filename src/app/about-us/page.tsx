4ade7e48b16fca68a901b287e32a28a99b2fd397
Andrick <Andrickjohab@gmail.com>
2025-03-02 20:33:52 -0800

    link5

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+// AI-PROJECT/src/app/about-us/page.tsx
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>Acerca de Nosotros</h1>
+      <p>Esta es la página de Acerca de Nosotros.</p>
+    </div>
+  );
+}
